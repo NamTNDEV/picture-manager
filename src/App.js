cc6598fef8f29c17b6f26415f75fe78da8a4fe5e
NamTNDEV <namtndev312002@gmail.com>
2024-01-17 08:29:07 +0700

    #7 - Add Axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -8,10 +8,27 @@ import {
 
 import NotFound from "./components/NotFound";
 import Header from "./components/Header";
+import productApi from "api/productApi";
 
 const Photo = lazy(() => import("./features/photo"));
 
 function App() {
+  useEffect(() => {
+    const fetchProductList = async () => {
+      try {
+        const params = {
+          offset: 10,
+          limit: 10,
+        };
+        const response = await productApi.getAll(params);
+        console.log(response);
+      } catch (error) {
+        console.log("Failed to fetch product list: ", error);
+      }
+    };
+    fetchProductList();
+  }, []);
+
   return (
     <div className="">
       <Suspense fallback={<div>Loading ...</div>}>
